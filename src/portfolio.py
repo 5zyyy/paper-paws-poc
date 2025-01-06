ca354342e7fc7b76a12cf6d36bb04ad7beafa112
wailok <auwailok1170@gmail.com>
2025-01-07 00:07:38 +0800

    portfolio changes

--- src/portfolio.py
+++ src/portfolio.py
@@ -5,5 +5,10 @@ from helpers.database_helper import fetch_data
 st.title("📊 Portfolio", anchor=False)
 
 if os.path.exists('trades.ddb'):
-    df = fetch_data("SELECT * FROM WHERE remaining = 0")
-    st.dataframe(df)
+    st.subheader("Opened Positions", anchor=False)
+    opened_df = fetch_data("SELECT * FROM positions WHERE remaining > 0")
+    st.dataframe(opened_df)
+
+    st.subheader("Closed Positions", anchor=False)
+    closed_df =  fetch_data("SELECT * FROM positions WHERE remaining = 0")
+    st.dataframe(closed_df)
