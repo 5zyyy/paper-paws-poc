7fd4a9ea82fa33cee047ed5e9e220620fe009062
wailok <auwailok1170@gmail.com>
2025-01-19 00:34:07 +0800

    modified database function parameters to allow change in db_name

--- src/helpers/database_helper.py
+++ src/helpers/database_helper.py
@@ -2,11 +2,11 @@ import duckdb
 import os
 import streamlit as st
 
-def create_db():
+def create_db(db_name='trades.ddb'):
     """
     Create the database and tables if they do not exist.
     """
-    with duckdb.connect('trades.ddb') as con:
+    with duckdb.connect(db_name) as con:
         con.execute(f'''
             CREATE TABLE IF NOT EXISTS transactions (
                 date DATE,
@@ -42,13 +42,14 @@ def create_db():
             );
         ''')
 
-def delete_db():
+def delete_db(db_name='trades.ddb'):
     """
     Delete the database file.
     """
-    os.remove('trades.ddb')
+    if os.path.exists(db_name):
+        os.remove(db_name)
 
-def insert_to_db(table, data):
+def insert_to_db(table, data, db_name='trades.ddb'):
     """
     Insert data into the specified table.
 
@@ -56,7 +57,7 @@ def insert_to_db(table, data):
     - table (str): The table to insert data into.
     - data (list): The data to insert.
     """
-    with duckdb.connect('trades.ddb') as con:
+    with duckdb.connect(db_name) as con:
         if table == 'transactions':
             query = f'''
                 INSERT INTO {table} (date, time, symbol, token, contract_address, action, market_cap, token_price, token_amt, total)
@@ -71,7 +72,7 @@ def insert_to_db(table, data):
 
         con.executemany(query, data)
 
-def fetch_data(query):
+def fetch_data(query, db_name='trades.ddb'):
     """
     Fetch data from the database using a SQL query.
 
@@ -81,7 +82,7 @@ def fetch_data(query):
     Returns:
     - pd.DataFrame: The result of the query as a DataFrame.
     """
-    with duckdb.connect('trades.ddb') as con:
+    with duckdb.connect(db_name) as con:
         data = con.sql(query).df()
     return data
 
@@ -125,14 +126,14 @@ def fetch_data_paginated(query, page_number, total_rows, type):
     df = fetch_data(query)
     return df, total_pages, False
 
-def delete_position(contract_address):
+def delete_position(contract_address, db_name='trades.ddb'):
     """
     Delete positions from the database based on contract address.
 
     Parameters:
     - contract_address (str or list): The contract address(es) to delete.
     """
-    with duckdb.connect('trades.ddb') as con:
+    with duckdb.connect(db_name) as con:
         contract_address_str = ""
         if isinstance(contract_address, list):
             for index, ca in enumerate(contract_address):
